a050b80de597a1aac3456098d3ba3a8b3fcb2211
Tanu <tanu.saxena0112@gmail.com>
2021-03-29 21:10:19 -0700

    first comment

---
+++ src/test/java/qaclickacademy/MobileTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+
+	@Test
+	public void MobileTest()
+	{
+		System.out.println("Mobile TESt");
+	}
+	
+
+	}
+
+
---
+++ src/test/java/qaclickacademy/REstAPITest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class REstAPITest {
+
+	@Test
+	public void RestAPI()
+	{
+		System.out.println("Testing Rest API");
+	}
+
+	}
+
+
